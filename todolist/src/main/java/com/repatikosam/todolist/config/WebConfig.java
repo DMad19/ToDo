30b0760ba6aa66a4607cb55ae93110e705d09a6f
DMad19 <dineshmadduru98813@gmail.com>
2025-04-06 13:59:05 +0530

    Change cors port

--- todolist/src/main/java/com/repatikosam/todolist/config/WebConfig.java
+++ todolist/src/main/java/com/repatikosam/todolist/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Allow frontend origin
+                .allowedOrigins("http://localhost:3000") // Allow frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow HTTP methods you need
                 .allowedHeaders("*"); // Allow all headers
     }
