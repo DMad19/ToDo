58b6da6a05e0ebec8b77868db05798adbb59ae3b
DMad19 <dineshmadduru98813@gmail.com>
2025-04-06 13:59:52 +0530

    Revamp frontend to next

--- frontend/src/context/UseFormVisibleContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-
-const formVisibilityContext = createContext({
-	isFormVisible: false,
-	toggleInputForm: () => {},
-});
-
-export const FormVisibilityContextProvider = formVisibilityContext.Provider;
-export const UseFormVisibilityContext = () => useContext(formVisibilityContext);
--- next_frontend/src/app/components/index.js
+++
@@ -1,7 +0,0 @@
-import Inputform from "./Inputform";
-import Task from "./Task";
-import Tasklist from "./Tasklist";
-import NewTaskBtn from "./NewTaskBtn";
-import Header from "./Header";
-import ThemeBtn from "./ThemeBtn";
-export { Inputform, Task, Tasklist, NewTaskBtn, Header, ThemeBtn };
